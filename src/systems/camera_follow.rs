6496ff6fd823217037a52f619e4fed77ef13daa5
Ole A. Sjo Fasting <oasf@polygo.no>
2020-05-10 10:59:08 +0200

    horizontal follow cam

--- src/systems/camera_follow.rs
+++ src/systems/camera_follow.rs
@@ -17,6 +17,7 @@ const HORIZONTAL_BOUNDS_DIRECTION_MODIFIER: f32 = 0.25;
 const HORIZONTAL_BOUNDS_MAX_PIXELS: f32 = 800.0;
 
 const VERTICAL_BOUNDS_SCREEN_FRACTION: f32 = 0.33;
+const VERTICAL_BOUNDS_DIRECTION_MODIFIER: f32 = 0.25;
 const VERTICAL_BOUNDS_MAX_PIXELS: f32 = 450.0;
 
 const MAX_FOLLOW_SPEED: f32 = 256.0;
@@ -38,25 +39,38 @@ impl<'s> System<'s> for CameraFollowSystem {
     &mut self,
     (mut transforms, actor_datas, player_actors, active_camera, screen_dimensions, physics_time): Self::SystemData,
   ) {
-    // TODO: LIMIT EXTENTS
-    let half_horizontal_extent =
-      (screen_dimensions.height() * VERTICAL_BOUNDS_SCREEN_FRACTION) / 2.0;
-    let (short_horizontal_extent, long_horizontal_extent) = {
-      let half_extent = (screen_dimensions.width() * HORIZONTAL_BOUNDS_SCREEN_FRACTION) / 2.0;
-      (
-        half_extent / HORIZONTAL_BOUNDS_DIRECTION_MODIFIER,
-        half_extent + (half_extent + HORIZONTAL_BOUNDS_DIRECTION_MODIFIER),
-      )
-    };
-
     let mut camera_translation = transforms
       .get(active_camera.0)
       .expect("Transform of active camera could not be found!")
       .translation()
       .clone();
 
+    // TODO: LIMIT EXTENTS
+    let (short_vertical_extent, long_vertical_extent) = {
+      let half_extent = (screen_dimensions.height() * VERTICAL_BOUNDS_SCREEN_FRACTION) / 2.0;
+      (
+        half_extent / VERTICAL_BOUNDS_DIRECTION_MODIFIER,
+        half_extent * (half_extent * VERTICAL_BOUNDS_DIRECTION_MODIFIER),
+      )
+    };
+
+    let (short_horizontal_extent, long_horizontal_extent) = {
+      let half_extent = (screen_dimensions.width() * HORIZONTAL_BOUNDS_SCREEN_FRACTION) / 2.0;
+      (
+        half_extent / HORIZONTAL_BOUNDS_DIRECTION_MODIFIER,
+        half_extent + (half_extent * HORIZONTAL_BOUNDS_DIRECTION_MODIFIER),
+      )
+    };
+
     for (transform, actor_data, _) in (&transforms, &actor_datas, &player_actors).join() {
       let player_translation = transform.translation();
+
+      let (up_threshold, down_threshold) = {
+        (
+          camera_translation.y + long_vertical_extent,
+          camera_translation.y - short_vertical_extent,
+        )
+      };
 
       let (left_threshold, right_threshold) = if actor_data.facing_right {
         (
@@ -79,8 +93,8 @@ impl<'s> System<'s> for CameraFollowSystem {
         };
       }
 
-      if player_translation.y <= camera_translation.y - half_horizontal_extent
-        || player_translation.y >= camera_translation.y + half_horizontal_extent
+      if player_translation.y <= camera_translation.y - down_threshold
+        || player_translation.y >= camera_translation.y + up_threshold
       {
         let distance = (player_translation.y - camera_translation.y) * physics_time.delta_seconds();
         camera_translation.y += if distance < MAX_FOLLOW_SPEED {
